d128286a76816aa08730f78550866d4f2d9c5983
Edsel Caprice <caprice@mail.usf.edu>
2018-11-17 15:31:28 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,7 @@ class ApplicationController < Sinatra::Base
    erb :new
  end
 
- post '/recipes' do
-   @recipe = Recipe.create(params)
-    redirect to "/recipes/#{@recipe.id}"
- end
+
 
  get '/recipes/:id' do
    @recipe = Recipe.find_by_id(params[:id])
@@ -36,6 +33,11 @@ class ApplicationController < Sinatra::Base
   @recipe.cook_time = params[:cook_time]
   @recipe.save
   redirect to "/recipes/#{@recipe.id}"
+end
+
+post '/recipes' do
+  @recipe = Recipe.create(params)
+   redirect to "/recipes/#{@recipe.id}"
 end
 
   delete '/recipes/:id/delete' do
